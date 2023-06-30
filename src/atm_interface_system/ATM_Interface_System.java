d0c824ba3d765ea7690fe6a4bda64926040c6f18
HP <HP@DESKTOP-QF1CU8C>
2023-06-30 23:08:10 +0530

    ATM_Interface_System_Java_Project (Code Clause Internship Project)

---
+++ src/atm_interface_system/ATM_Interface_System.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atm_interface_system;
+
+/**
+ *
+ * @author HP
+ */
+public class ATM_Interface_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
